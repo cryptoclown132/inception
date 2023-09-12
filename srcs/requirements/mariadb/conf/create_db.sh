f86e65a515b4f636bd6eb87c1f5db5a156fc0f0e
jkroger <jkroger@student.42wolfsburg.de>
2023-09-12 18:29:23 +0200

    12

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,36 +1,24 @@
-#!/bin/bash
+#!bin/sh
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-	chown -R mysql:mysql /var/lib/mysql
 
-	# init database
-	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
-	
-	tfile=`mktemp`
-	if [ ! -f "$tfile" ]; then
-		return 1
-	fi
+        chown -R mysql:mysql /var/lib/mysql
+
+        # init database
+        mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
+
+        tfile=`mktemp`
+        if [ ! -f "$tfile" ]; then
+                return 1
+        fi
 fi
 
-
-
-
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
-mysql_secure_installation <<_EOF_
 
-Y
-root4life
-root4life
-Y
-n
-Y
-Y
-_EOF_
-
-	cat << EOF > /tmp/create_db.sql
+        cat << EOF > /tmp/create_db.sql
 USE mysql;
 FLUSH PRIVILEGES;
-DELETE FROM mysql.user WHERE User='';
+DELETE FROM     mysql.user WHERE User='';
 DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
@@ -39,10 +27,8 @@ CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
 GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 EOF
-	# run init.sql
-	/usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
-	rm -f /tmp/create_db.sql
+        # run init.sql
+        /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
+        rm -f /tmp/create_db.sql
 fi
-
