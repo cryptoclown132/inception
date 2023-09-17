16556fe87c0855d26cc041fd6a12576901313edf
jkroger <jkroger@student.42wolfsburg.de>
2023-09-17 10:11:24 +0200

    db annother one

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,76 +1,38 @@
 #!/bin/sh
 
-mysql_install_db
+if [ ! -d "/var/lib/mysql/mysql" ]; then
 
-/etc/init.d/mysql start
+    chown -R mysql:mysql /var/lib/mysql
 
-#Check if the database exists
+    # init database
+    mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
 
-if [ -d "/var/lib/mysql/$DB_NAME" ]
-then 
-
-	echo "Database already exists"
-else
-
-# Set root option so that connexion without root password is not possible
-
-mysql_secure_installation <<_EOF_
-
-Y
-root4life
-root4life
-Y
-n
-Y
-Y
-_EOF_
-
-#Add a root user on 127.0.0.1 to allow remote connexion
-
-	echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT'; FLUSH PRIVILEGES;" | mysql -uroot
-
-#Create database and user for wordpress
-	echo "CREATE DATABASE IF NOT EXISTS $DB_NAME; GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'; FLUSH PRIVILEGES;" | mysql -uroot
-
-#Import database
-mysql -uroot -p$DB_ROOT $DB_NAME < /usr/local/bin/wordpress.sql
-
+    tfile=$(mktemp)
+    if [ ! -f "$tfile" ]; then
+        return 1
+    fi
 fi
 
-/etc/init.d/mysql stop
+if [ ! -d "/var/lib/mysql/wordpress" ]; then
 
-exec "$@"
-# #!bin/sh
+    cat <<EOF > /tmp/create_db.sql
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
+CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
+GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
+-- Create root user with a password
+CREATE USER 'root'@'%' IDENTIFIED BY '${DB_ROOT}';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
 
-# if [ ! -d "/var/lib/mysql/mysql" ]; then
-
-#         chown -R mysql:mysql /var/lib/mysql
-
-#         # init database
-#         mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
-
-#         tfile=`mktemp`
-#         if [ ! -f "$tfile" ]; then
-#                 return 1
-#         fi
-# fi
-
-# if [ ! -d "/var/lib/mysql/wordpress" ]; then
-
-#         cat << EOF > /tmp/create_db.sql
-# USE mysql;
-# FLUSH PRIVILEGES;
-# DELETE FROM     mysql.user WHERE User='';
-# DROP DATABASE test;
-# DELETE FROM mysql.db WHERE Db='test';
-# DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
-# CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
-# CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
-# GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
-# FLUSH PRIVILEGES;
-# EOF
-#         # run init.sql
-#         /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
-#         rm -f /tmp/create_db.sql
-# fi
+    # run init.sql
+    /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
+    rm -f /tmp/create_db.sql
+fi
